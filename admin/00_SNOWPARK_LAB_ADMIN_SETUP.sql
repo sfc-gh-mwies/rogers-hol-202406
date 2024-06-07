9a9f6db42253edba21f650eea9699347045e9fcc
Mike Wies <mike.wies@snowflake.com>
2024-06-07 13:45:35 -0400

    updated setup script to use share instead of ext stage load

--- admin/00_SNOWPARK_LAB_ADMIN_SETUP.sql
+++ admin/00_SNOWPARK_LAB_ADMIN_SETUP.sql
@@ -26,7 +26,7 @@ USE ROLE sysadmin;
 --*/
 
 -- create tb_101 database
-CREATE OR REPLACE DATABASE tb_101;
+CREATE OR REPLACE DATABASE tb_101 from share tb_101_share;
 
 -- create raw_pos schema
 CREATE OR REPLACE SCHEMA tb_101.raw_pos;
@@ -45,6 +45,7 @@ CREATE OR REPLACE WAREHOUSE tb_de_wh
     WAREHOUSE_SIZE = 'large' -- Large for initial data load - scaled down to XSmall at end of this scripts
     WAREHOUSE_TYPE = 'standard'
     AUTO_SUSPEND = 60
+    MAX_CLUSTER_COUNT = 10
     AUTO_RESUME = TRUE
     INITIALLY_SUSPENDED = TRUE
 COMMENT = 'data engineering warehouse for tasty bytes';
@@ -53,6 +54,7 @@ CREATE OR REPLACE WAREHOUSE tb_dev_wh
     WAREHOUSE_SIZE = 'xsmall'
     WAREHOUSE_TYPE = 'standard'
     AUTO_SUSPEND = 60
+    MAX_CLUSTER_COUNT = 10
     AUTO_RESUME = TRUE
     INITIALLY_SUSPENDED = TRUE
 COMMENT = 'developer warehouse for tasty bytes';
@@ -157,129 +159,35 @@ file_format = tb_101.public.csv_ff;
 
 -- country table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.country
-(
-    country_id NUMBER(18,0),
-    country VARCHAR(16777216),
-    iso_currency VARCHAR(3),
-    iso_country VARCHAR(2),
-    city_id NUMBER(19,0),
-    city VARCHAR(16777216),
-    city_population VARCHAR(16777216)
-);
+AS SELECT * FROM tb_101_share.raw_pos.country;
 
 -- franchise table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.franchise 
-(
-    franchise_id NUMBER(38,0),
-    first_name VARCHAR(16777216),
-    last_name VARCHAR(16777216),
-    city VARCHAR(16777216),
-    country VARCHAR(16777216),
-    e_mail VARCHAR(16777216),
-    phone_number VARCHAR(16777216) 
-);
+AS SELECT * FROM tb_101_share.raw_pos.franchise;
 
 -- location table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.location
-(
-    location_id NUMBER(19,0),
-    placekey VARCHAR(16777216),
-    location VARCHAR(16777216),
-    city VARCHAR(16777216),
-    region VARCHAR(16777216),
-    iso_country_code VARCHAR(16777216),
-    country VARCHAR(16777216)
-);
+AS SELECT * FROM tb_101_share.raw_pos.location;
 
 -- menu table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.menu
-(
-    menu_id NUMBER(19,0),
-    menu_type_id NUMBER(38,0),
-    menu_type VARCHAR(16777216),
-    truck_brand_name VARCHAR(16777216),
-    menu_item_id NUMBER(38,0),
-    menu_item_name VARCHAR(16777216),
-    item_category VARCHAR(16777216),
-    item_subcategory VARCHAR(16777216),
-    cost_of_goods_usd NUMBER(38,4),
-    sale_price_usd NUMBER(38,4),
-    menu_item_health_metrics_obj VARIANT
-);
+AS SELECT * FROM tb_101_share.raw_pos.menu;
 
 -- truck table build 
 CREATE OR REPLACE TABLE tb_101.raw_pos.truck
-(
-    truck_id NUMBER(38,0),
-    menu_type_id NUMBER(38,0),
-    primary_city VARCHAR(16777216),
-    region VARCHAR(16777216),
-    iso_region VARCHAR(16777216),
-    country VARCHAR(16777216),
-    iso_country_code VARCHAR(16777216),
-    franchise_flag NUMBER(38,0),
-    year NUMBER(38,0),
-    make VARCHAR(16777216),
-    model VARCHAR(16777216),
-    ev_flag NUMBER(38,0),
-    franchise_id NUMBER(38,0),
-    truck_opening_date DATE
-);
+AS SELECT * FROM tb_101_share.raw_pos.truck;
 
 -- order_header table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.order_header
-(
-    order_id NUMBER(38,0),
-    truck_id NUMBER(38,0),
-    location_id FLOAT,
-    customer_id NUMBER(38,0),
-    discount_id VARCHAR(16777216),
-    shift_id NUMBER(38,0),
-    shift_start_time TIME(9),
-    shift_end_time TIME(9),
-    order_channel VARCHAR(16777216),
-    order_ts TIMESTAMP_NTZ(9),
-    served_ts VARCHAR(16777216),
-    order_currency VARCHAR(3),
-    order_amount NUMBER(38,4),
-    order_tax_amount VARCHAR(16777216),
-    order_discount_amount VARCHAR(16777216),
-    order_total NUMBER(38,4)
-);
+AS SELECT * FROM tb_101_share.raw_pos.order_header;
 
 -- order_detail table build
 CREATE OR REPLACE TABLE tb_101.raw_pos.order_detail 
-(
-    order_detail_id NUMBER(38,0),
-    order_id NUMBER(38,0),
-    menu_item_id NUMBER(38,0),
-    discount_id VARCHAR(16777216),
-    line_number NUMBER(38,0),
-    quantity NUMBER(5,0),
-    unit_price NUMBER(38,4),
-    price NUMBER(38,4),
-    order_item_discount_amount VARCHAR(16777216)
-);
+AS SELECT * FROM tb_101_share.raw_pos.counorder_detailtry;
 
 -- customer loyalty table build
 CREATE OR REPLACE TABLE tb_101.raw_customer.customer_loyalty
-(
-    customer_id NUMBER(38,0),
-    first_name VARCHAR(16777216),
-    last_name VARCHAR(16777216),
-    city VARCHAR(16777216),
-    country VARCHAR(16777216),
-    postal_code VARCHAR(16777216),
-    preferred_language VARCHAR(16777216),
-    gender VARCHAR(16777216),
-    favourite_brand VARCHAR(16777216),
-    marital_status VARCHAR(16777216),
-    children_count VARCHAR(16777216),
-    sign_up_date DATE,
-    birthday_date DATE,
-    e_mail VARCHAR(16777216),
-    phone_number VARCHAR(16777216)
-);
+AS SELECT * FROM tb_101_share.raw_pos.customer_loyalty;
 
 /*--
  • harmonized view creation
